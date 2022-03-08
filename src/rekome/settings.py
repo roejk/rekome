80f9528b0c1eba521c6564758b8708c9b374b156
Piotrek <piotrek.piotr2@gmail.com>
2022-03-08 06:38:39 +0100

    Disable Django debug mode

--- src/rekome/settings.py
+++ src/rekome/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8020', os.getenv('CSRF_ORIGIN')]
