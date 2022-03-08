eb0aef05cb64a033c71cb93a73ef4662fb490a46
Piotrek <piotrek.piotr2@gmail.com>
2022-03-08 07:30:51 +0100

    Add static files collection

--- start-server.sh
+++ start-server.sh
@@ -2,5 +2,5 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (cd rekome; python manage.py createsuperuser --no-input)
 fi
-(cd rekome; gunicorn rekome.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
+(cd rekome; python manage.py collectstatic --noinput; gunicorn rekome.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
 nginx -g "daemon off;"
